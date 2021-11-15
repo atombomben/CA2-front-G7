17660139d0908e014b61f819b70c16048f239056
atombomben <71074932+atombomben@users.noreply.github.com>
2021-11-15 17:28:48 +0100

    updated Tomcat to tomcat
    stupid mistakes :(

--- ca2/src/Settings.js
+++ ca2/src/Settings.js
@@ -1,10 +1,11 @@
-//Local http://localhost:8080/CA2
+const URL="https://atombomben.dk/tomcat/CA2"
 
-const URL="https://atombomben.dk/Tomcat/CA2"
-
+//const URL="http://localhost:8080/CA2"
 
 //Local "http://localhost:8080/CA2/api/weather"
 
-const WEATHER_URL="https://atombomben.dk/Tomcat/CA2/api/weather"
+const WEATHER_URL="https://atombomben.dk/tomcat/CA2/api/weather"
+
+//const WEATHER_URL="http://localhost:8080/CA2/api/weather"
 
 export { URL, WEATHER_URL };
